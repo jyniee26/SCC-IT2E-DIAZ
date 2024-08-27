89c2e377c09cd7c7f99cc7132d1fd6f7d9dd3aa4
jyniee26 <romojynieelyn@gmail.com>
2024-08-27 23:03:32 +0800

    it is hard

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,9 @@
+package javaapplication2;
+
+public class JavaApplication2 {
+    public static void main(String[] args) {
+        Gradeclass gr = new Gradeclass();
+        gr.addGrades(1011, "Mike Gwapo", 1.0, 1.2, 1.3, 1.4);
+        gr.viewGrades();
+    }
+}
