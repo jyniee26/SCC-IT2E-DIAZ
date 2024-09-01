bead2ad66e895a18506aeb0a251ccd3c63b4a358
jyniee26 <romojyniee@gmail.com>
2024-08-31 23:50:20 -1200

    okay

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -4,8 +4,10 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         Grade gr = new Grade();
         Product pr = new Product();
+        Account acc = new Account();
         
 //        gr.getGrades();
-        pr.getProducts();
+//        pr.getProducts();
+        acc.getAccount();
     }
 }
